782ae9660d599d6d4faefee4f904a3ca3a8d484e
ClownPier <114249040+ClownPier@users.noreply.github.com>
2022-12-28 12:10:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var first_name = "Ranbir";
+var second_name = "Kapoor";
+
+
+var a = 10;
+var b = 5;
+
+
+var add = a + b;
+console.log(add);
+
+var subtract = a - b;
+console.log(subtract);
+
+var multiply = a * b;
+console.log(multiply);
+
+var divide = a / b;
+console.log(divide);
